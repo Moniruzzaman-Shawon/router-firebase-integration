9f163358e292dd5b0318858f461fefa46e977deb
Moniruzzaman-Shawon <m.zaman.djp@gmail.com>
2024-05-22 04:17:27 +0600

    debugging

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,25 +2,28 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
 import useFirebase from '../../hooks/useFirebase';
-import handleSignOut from '../../hooks/useFirebase'
+import handleSignOut from '../../hooks/useFirebase';
+
 
 const Header = () => {
-    const { user } = useFirebase();
+    const { user, handleSignOut } = useFirebase();
     return (
         <div className='header'>
             <nav>
                 <Link to='/'>Home</Link>
                 <Link to='/products'>Products</Link>
                 <Link to='/orders'>Orders</Link>
+                <Link to='/register'>Register</Link>
                 <span>{user.displayName && user.displayName}</span>
                 {
-                    <>
 
+                    user.uid
+                        ?
                         <button onClick={handleSignOut}>Sign out</button>
+                        :
 
-                        <Link to='/login'>Login</Link></>
+                        <Link to='/login'>Login</Link>
                 }
-                <Link to='/register'>Register</Link>
             </nav>
         </div>
     );
