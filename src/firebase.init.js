31061c6a755d3261611ce6f12a8df17464e3b51a
Moniruzzaman-Shawon <m.zaman.djp@gmail.com>
2024-05-22 02:08:46 +0600

    header and route added & header style added

---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAzbZkdjnOy89B_bLh2nl_OsFZE1c8CVAA",
+    authDomain: "router-firebase-intregat-cb479.firebaseapp.com",
+    projectId: "router-firebase-intregat-cb479",
+    storageBucket: "router-firebase-intregat-cb479.appspot.com",
+    messagingSenderId: "766196994754",
+    appId: "1:766196994754:web:3bbdc692380993c022a15b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
