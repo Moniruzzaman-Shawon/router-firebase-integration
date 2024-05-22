1ebbe8156219add4ac39fc76fb7e39a36e96e1f2
Moniruzzaman-Shawon <m.zaman.djp@gmail.com>
2024-05-22 20:23:34 +0600

    Implement auth direct

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,18 +1,29 @@
 import React from 'react';
-import useFirebase from '../../hooks/useFirebase';
 import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import { getAuth } from 'firebase/auth';
 import app from '../../firebase.init';
+import { useLocation, useNavigate } from 'react-router';
 
 const auth = getAuth(app);
 
 const Login = () => {
     const [signInWithGoogle, user] = useSignInWithGoogle(auth);
+    const location = useLocation();
+    const navigate = useNavigate();
+    const from = location?.state?.from?.pathname || '/';
+
+    const handleGoogleSignIn = () => {
+        signInWithGoogle()
+            .then(() => {
+                navigate(from, { replace: true })
+            })
+    }
+
     return (
         <div>
             <h3>Please login</h3>
             <div style={{ margin: '20px' }}>
-                <button onClick={() => signInWithGoogle()}>Google Sign in</button>
+                <button onClick={handleGoogleSignIn}>Google Sign in</button>
             </div>
 
             <form>
